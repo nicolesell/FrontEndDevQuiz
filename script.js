c28ae2c1ce2fc4e5ab84012c11860df0c49cbe72
nicolesell <na23sell@siena.edu>
2022-10-12 01:24:37 -0400

    Fixing HTML

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 
-// phone formatter function
 $(document).ready(function(){
+    
+    //phone format with inputmask
    var phone = [{ "mask": "(###) ###-####"}];
     $('#phone').inputmask({ 
         mask: phone, 
         definitions: { '#': { validator: "[0-9]"}} });
+
+    // 
+    $( "form" ).submit(function( event ) {
+        console.log($(this).serializeArray() );
+        });
 });
+
